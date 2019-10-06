a152ca723b108350c324a7f568381a175f4da23a
Granite Bagas <granitebagas28@gmail.com>
2019-10-06 17:10:58 +0700

    Test

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ mongoose.connect(
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get("/", (req, res) => {
+  res.send("Test");
+});
+
 app.use("/api", require("./routes/post"));
 
 const port = process.env.PORT || 3000;
